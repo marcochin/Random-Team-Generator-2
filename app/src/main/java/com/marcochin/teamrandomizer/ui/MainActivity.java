dd0a733a52ad9e4a30702b050852ee07d6fcca78
Marco Chin <marcochin@142-254-51-105.dsl.dynamic.fusionbroadband.com>
2019-09-05 22:49:48 -0700

    figured out integration of bottomNavView with navController and created player name item

--- app/src/main/java/com/marcochin/teamrandomizer/ui/MainActivity.java
+++ app/src/main/java/com/marcochin/teamrandomizer/ui/MainActivity.java
@@ -2,11 +2,9 @@ package com.marcochin.teamrandomizer.ui;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.view.MenuItem;
 
-import androidx.annotation.NonNull;
-import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.marcochin.teamrandomizer.R;
@@ -14,7 +12,6 @@ import com.marcochin.teamrandomizer.R;
 import dagger.android.support.DaggerAppCompatActivity;
 
 public class MainActivity extends DaggerAppCompatActivity {
-    private NavController mNavController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,27 +19,12 @@ public class MainActivity extends DaggerAppCompatActivity {
         setContentView(R.layout.activity_main);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.main_bottom_nav);
-        bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        mNavController = Navigation.findNavController(this, R.id.main_nav_host_fragment);
+        // Connect bottomNavView with the navController and it will know which fragment to load
+        // as long as the bottomNavView menu item ids match the fragment ids in the nav_graph.xml
+        NavigationUI.setupWithNavController(bottomNavigationView,
+                Navigation.findNavController(this, R.id.main_nav_host_fragment));
     }
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-            switch (menuItem.getItemId()) {
-                case R.id.nav_randomize:
-                    mNavController.navigate(R.id.action_loadFragment_to_addPlayersFragment);
-                    break;
-                case R.id.nav_load:
-                    mNavController.navigate(R.id.action_addPlayersFragment_to_loadFragment);
-                    break;
-            }
-
-            return true; // Return true if you want the bottom nav button to be selected
-        }
-    };
 
     @Override
     public void onBackPressed() {
